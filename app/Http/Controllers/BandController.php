3dbc72d35a5a54f2e967be854a51379a2fb66365
Alexisandro Ribeiro <alexetec.ribeiro@gmail.com>
2024-02-26 02:30:19 -0300

    criando model e comuninando com banco trazendo retorno do banco de dadps

--- app/Http/Controllers/BandController.php
+++ app/Http/Controllers/BandController.php
@@ -3,35 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Band; 
 
 class BandController extends Controller
 {
     public function getAll()
     {
-        $bands = $this->getBands();
-
+        $bands = Band::all(); 
         return response()->json($bands);
     }
 
     public function getById($id)
     {
-        $bands = $this->getBands();
+        $band = Band::find($id);
 
-        $band = array_filter($bands, function ($band) use ($id) {
-            return $band["id"] == $id;
-        });
-
-        return response()->json(array_values($band));
+        return response()->json($band);
     }
-
-    // protected function getBands()
-    // {
-    //     return [
-    //         ["id" => 1, "name" => "AC/DC", "gender" => "Rock", "year" => 1973],
-    //         ["id" => 2, "name" => "Metallica", "gender" => "Rock", "year" => 1981],
-    //         ["id" => 3, "name" => "Ramones", "gender" => "Punk", "year" => 1974],
-    //         ["id" => 4, "name" => "Queen", "gender" => "Rock", "year" => 1970],
-    //         ["id" => 5, "name" => "Guns N Roses", "gender" => "Rock", "year" => 1985]
-    //     ];
-    // }
 }
